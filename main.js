13db4ae56633fab21dfd9e6f8b0e1f48544bfa25
Kirikuyaba <118457909+Kirikuyaba@users.noreply.github.com>
2022-12-15 11:10:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+       window.onload = function(){
+         var contenedor = document.getElementById('contenedor_carga');
+         
+             contenedor.style.visibility = 'hidden';
+             contenedor.style.opacity = '0';
+             }
+           
+           
+             ;
+             
+       
+               
+
+              
+             
+   
+         
